2dde0b5f6d975bb6a2a34318218b298aba72b627
yujunquan <yujunquan@sinorock.work>
2020-06-04 10:52:11 +0800

    2020年6月4日10:30:44 项目结构整理2

---
+++ projects/ddd/src/main/java/com/yujunquan/studylife/projects/ddd/DDDApplication.java
@@ -0,0 +1,13 @@
+package com.yujunquan.studylife.projects.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDDApplication.class, args);
+    }
+
+}
---
+++ projects/ddd/src/main/java/com/yujunquan/studylife/projects/ddd/entity/UserInfo.java
@@ -0,0 +1,9 @@
+package com.yujunquan.studylife.projects.ddd.entity;
+
+public class UserInfo {
+    private String userInfoID;
+    private String name;
+    private String userID;
+
+
+}
